8aeff811b2ff4d49db042b9c22e5b8ccae78e6d0
Pedro Paulo Galvão <52977365+PPMAKER@users.noreply.github.com>
2022-05-16 18:13:09 -0300

    Add files via upload

---
+++ src/test/java/com/mycompany/introjunit5/MathUtilMdcTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.introjunit5;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Classe para inclusão dos testes unitários para
+ * os métodos mdc() da classe {@link MathUtil},
+ * que calcula o Máximo Divisor Comum de dois ou mais números.
+ *
+ * <p>Cada teste unitário deve verificar uma determinada propriedade
+ * do MDC. Os métodos de teste indicam o número da 
+ * <a href="https://pt.wikipedia.org/wiki/Máximo_divisor_comum#Propriedades">propriedade<</a> 
+ * testada (como p1) no nome.</p>
+ * 
+ * @author Manoel Campos da Silva Filho <http://github.com/manoelcampos>
+ */
+class MathUtilMdcTest {
+
+
+}
