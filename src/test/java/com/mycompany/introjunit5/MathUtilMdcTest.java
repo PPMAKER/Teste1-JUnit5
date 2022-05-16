04d2389c8587a6a15c0c66e2fe28005b14e9872a
Pedro Paulo Galvão <52977365+PPMAKER@users.noreply.github.com>
2022-05-16 18:15:48 -0300

    Update MathUtilMdcTest.java

--- src/test/java/com/mycompany/introjunit5/MathUtilMdcTest.java
+++ src/test/java/com/mycompany/introjunit5/MathUtilMdcTest.java
@@ -8,18 +8,6 @@ package com.mycompany.introjunit5;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Classe para inclusão dos testes unitários para
- * os métodos mdc() da classe {@link MathUtil},
- * que calcula o Máximo Divisor Comum de dois ou mais números.
- *
- * <p>Cada teste unitário deve verificar uma determinada propriedade
- * do MDC. Os métodos de teste indicam o número da 
- * <a href="https://pt.wikipedia.org/wiki/Máximo_divisor_comum#Propriedades">propriedade<</a> 
- * testada (como p1) no nome.</p>
- * 
- * @author Manoel Campos da Silva Filho <http://github.com/manoelcampos>
- */
 class MathUtilMdcTest {
 
 
